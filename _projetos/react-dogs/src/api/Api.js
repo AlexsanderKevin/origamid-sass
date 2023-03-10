230547f84a129de6c1f6f073d3f9d69f33f4a10b
Kevin Jesus <kevin13348@gmail.com>
2023-03-10 14:43:52 -0300

    tested all endpoints

--- _projetos/react-dogs/src/api/Api.js
+++ _projetos/react-dogs/src/api/Api.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import PhotoGet from './endpoints/PhotoGet'
+import PhotoPost from './endpoints/PhotoPost'
 import TokenPost from './endpoints/TokenPost'
 import UserPost from './endpoints/UserPost'
 
@@ -7,8 +9,15 @@ const Api = () => {
     <div>
       <h2>USER POST</h2>
       <UserPost/>
+
       <h2>TOKEN POST</h2>
       <TokenPost/>
+
+      <h2>PHOTO POST</h2>
+      <PhotoPost/>
+
+      <h2>PHOTO GET</h2>
+      <PhotoGet/>
     </div>
   )
 }
